942b210e3ff2fc13ebd19a4d91a28f9da437b608
cliu68 <chunyao.liu0608@gmail.com>
2023-04-16 19:46:32 -0700

    update buildings.sql

--- buildings.sql
+++ buildings.sql
@@ -5,7 +5,6 @@ shortname  varchar(10) unique,
 id integer primary key AUTOINCREMENT
 
 );
-
 create table rooms 
 (
 number integer,
@@ -14,3 +13,9 @@ seating integer,
 primary key (number, buildingid),
 foreign key (buildingid) references buildings(id)
 );
+
+insert into buildings (name, shortname) VALUES ('Global Scholars Hall', 'GSH');
+insert into buildings (name, shortname) VALUES ('Justice Bean Hall', 'JBH');
+insert into rooms (number, buildingid, seating) values (399, (select id from buildings where shortname = 'GSH'), 26);
+insert into rooms (number, buildingid, seating) values (416, (select id from buildings where shortname = 'JBH'), 33);
+
